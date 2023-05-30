fd45d316256f06b6c2ffa3d7112dbf23bd50ea69
Shaidul Islam <mdshaidulicse@gmail.com>
2023-05-30 07:34:08 +0600

    commit 2nd

---
+++ app/src/main/java/com/ssskot/tictactoeshaidul/kotlinfiles/GameActivity.kt
@@ -0,0 +1,11 @@
+package com.ssskot.tictactoeshaidul.kotlinfiles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
